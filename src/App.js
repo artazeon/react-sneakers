fbd52f36ab22a37996c730702ee5894aa7e4b91b
artazeon <artazeon@gmail.com>
2025-02-18 17:01:12 +0300

    update react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img src="/img/logo.png" alt="" />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кросовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img width={18} height={18} src="/img/cart.svg" alt="" />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/favorites.svg" alt="" />
+            <span>Закладки</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/user.svg" alt="" />
+            <span>Профиль</span>
+          </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Все кросовки</h1>
+        ...
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
