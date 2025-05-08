80da64f82854c3cf94871b87a14231c3342c5b6d
artazeon <artazeon@gmail.com>
2025-05-08 19:30:05 +0300

    fix bug: router deploy

--- src/App.js
+++ src/App.js
@@ -151,7 +151,7 @@ function App() {
 
         <Routes>
           <Route
-            path=""
+            path="react-sneakers"
             element={
               <Home
                 items={items}
